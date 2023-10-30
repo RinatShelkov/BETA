2f6100736a2f67368d11e0bd35d73026d677d4f2
rinat shelkov <rinarserov@icloud.com>
2023-10-31 01:37:02 +0500

    Added in docstring description of parameters and returned values functions get_dict_with_key_state Add argument 'key' to function get_dict_with_key_state Renaming the list returned by a function(list_comrehantions) Added in docstring description of parameters and returned values functions get_sorted_list

--- src/processing.py
+++ src/processing.py
@@ -1,13 +1,16 @@
-def get_dict_with_key_state(list_dict: list) -> list:
+def get_dict_with_key_state(list_dict: list, key="EXECUTED") -> list:
     """Функция принимает список словарей и возвращает список словарей с ключом state = EXECUTED
-    :param list_dict
-    :return list_executed"""
+    :param
+    list_dict: Список словарей с операцийми для фильтрации
+    key: Ключ для фильтрации
+    :return
+    list_executed Список словарей отфильтрованный ключом key"""
 
-    list_executed = []
+    list_comrehantions = []
     for dictionary in list_dict:
-        if dictionary["state"] == "EXECUTED":
-            list_executed.append(dictionary)
-    return list_executed
+        if dictionary["state"] == key:
+            list_comrehantions.append(dictionary)
+    return list_comrehantions
 
 
 def get_sorted_list(list_dict: list, revers: bool = True) -> list:
@@ -15,9 +18,10 @@ def get_sorted_list(list_dict: list, revers: bool = True) -> list:
     отсортированы по убыванию даты(ключ date). Функция принмиает два аргумента, второй необязательный
     задает порядок сортировки(убывание, возраcтание)
     :param
-    list_dict
-    revers
-    :return sorted_list"""
+    list_dict: Список словарей с операциями для фильтрации по дате
+    revers: bool значение для выбора сортировки(убывание/возрастание)
+    :return
+    sorted_list Список словарей отсоритированных по атрибуту revers"""
 
     sorted_list = sorted(list_dict, key=lambda date: date["date"], reverse=revers)
     return sorted_list

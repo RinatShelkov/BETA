4b4c47857ecaef02ee98f3a149bf5048908ac3b2
rinat shelkov <rinarserov@icloud.com>
2023-10-30 21:57:51 +0500

    Add functions get_sorted_list

--- src/processing.py
+++ src/processing.py
@@ -8,3 +8,16 @@ def get_dict_with_key_state(list_dict: list) -> list:
         if dictionary["state"] == "EXECUTED":
             list_executed.append(dictionary)
     return list_executed
+
+
+def get_sorted_list(list_dict: list, revers: bool = True) -> list:
+    """Функция принимает список словарей и возвращает новый список в котором исходные словари
+    отсортированы по убыванию даты(ключ date). Функция принмиает два аргумента, второй необязательный
+    задает порядок сортировки(убывание, возраcтание)
+    :param
+    list_dict
+    revers
+    :return sorted_list"""
+
+    sorted_list = sorted(list_dict, key=lambda date: date["date"], reverse=revers)
+    return sorted_list

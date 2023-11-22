13edc665083caed090bc124b949a77fdc65c5ac2
rinat shelkov <rinarserov@icloud.com>
2023-11-22 23:57:43 +0500

    Add reading.py add def reading_csv_xlsx_file in reading.py Add pandas

--- data/config.py
+++ data/config.py
@@ -1,6 +1,5 @@
 from pathlib import Path
 
-
 ROOT_PATH = Path(__file__).resolve().parent.parent
 SRC_PATH = ROOT_PATH.joinpath("src")
 LOG_PATH = ROOT_PATH.joinpath("log")
@@ -9,3 +8,5 @@ TEST_UTILS_JSONDecodeError = ROOT_PATH.joinpath("data").joinpath("test_get_list_
 TEST_UTILS_FileNotFoundError = ROOT_PATH.joinpath("data").joinpath("test_get_list_dict_invalid")
 LOG_MASKS_PATH = ROOT_PATH.joinpath("log").joinpath("masks.log")
 LOG_UTILS_PATH = ROOT_PATH.joinpath("log").joinpath("utils.log")
+TRANSACTION_PATH_CSV = ROOT_PATH.joinpath("data").joinpath("transactions.csv")
+TRANSACTION_PATH_XLSX = ROOT_PATH.joinpath("data").joinpath("transactions_excel.xlsx")
